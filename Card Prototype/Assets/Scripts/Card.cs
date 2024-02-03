7d5425195aa760b99a4ed6e3f464594245cd943d
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-03 18:32:37 -0500

    Card class and database creation
    I created a card class and database that can be then be pulled in the future. Currently holds cards related to the Circus deck. Passes along 4 integer variables and 2 strings.

--- Card Prototype/Assets/Scripts/Card.cs
+++ Card Prototype/Assets/Scripts/Card.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variable list
+    public int id;
+    public int cost;
+    public int power;
+    public int health;
+    public string cardDescription;
+    public string cardName;   
+
+    public Card()
     {
-        
-        //Git test text
+
     }
 
-    // Update is called once per frame
-    void Update()
+  
+    public Card(int ID, int Mana, int Attack, int HP, string CardName, string CardDes)
     {
-        
+        //Will pass variables Through with new names
+        id = ID;
+        cost = Mana;
+        power = Attack;
+        health = HP;
+        cardName = CardName;
+        cardDescription = CardDes;
+
     }
 }
