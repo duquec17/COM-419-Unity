b72ec19a463d959a203fcb7eb68e1602ca020911
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-05 23:43:12 -0500

    update drop

--- Card Prototype/Assets/Scripts/DropZone.cs
+++ Card Prototype/Assets/Scripts/DropZone.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
