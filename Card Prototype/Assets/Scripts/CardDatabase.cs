1896e80486bd77d009d8945972c2e410f6b93ea3
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-13 18:47:10 -0500

    Update CardDatabase.cs

--- Card Prototype/Assets/Scripts/CardDatabase.cs
+++ Card Prototype/Assets/Scripts/CardDatabase.cs
@@ -1,69 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-
+using UnityEngine;
 
 public class CardDatabase : MonoBehaviour
 {
 
     public static List<Card> cardList = new List<Card>();
 
-    public int displayId;
-
-    public int id;
-    public string cardName;
-    public int cost;
-    public int power;
-    public int health;
-    public string cardDescription;
-    public Sprite spriteImage;
-
-    public TMP_Text nameText;
-    public TMP_Text costText;
-    public TMP_Text powerText;
-    public TMP_Text descriptionText;
-    public TMP_Text healthText;
-    public Image artImage;
-
-    public bool cardBack;
-    public static bool staticCardBack;
-
-    private void Awake()
+    void Awake()
     {
-        cardList.Add(new Card(0, "Example", 0, 0, 0, "broke", spriteImage));
+
+        cardList.Add(new Card(0, "Jester", 1, 1, 2, "+1 power for every other Jester card on the board", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(1, "Jester", 1, 1, 2, "+1 power for every other Jester card on the board", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(2, "Jester", 1, 1, 2, "+1 power for every other Jester card on the board", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(3, "One Man Circus", 4, 0, 4, "Players health cannot be damaged until this card is destroyed", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(4, "Knife Thrower", 3, 1, 2, "Attacks three seperate lanes", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(5, "Psycho Clown", 3, 4, 1, "+1 power every time an ally card is destroyed", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(6, "Volunteer", 1, 1, 1, "Moves to an empty lane to protect from incoming damage", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(7, "Lion Tamer", 8, 2, 2, "Spawns 2 4|4 Tiger cards in the players hand", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(8, "Ring Leader", 10, 4, 4, "+2 health to all allies on the board", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(9, "Trapeze Artist", 6, 2, 4, "Attacks over blocking units", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(10, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(11, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(12, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(13, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(14, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(15, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(16, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(17, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(18, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
+        cardList.Add(new Card(19, "None", 0, 0, 0, "None", Resources.Load<Sprite>("placeHolder")));
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        displayId = 0;
-        id = cardList[0].id;
-        cardName = cardList[0].cardName;
-        cost = cardList[0].cost;
-        power = cardList[0].power;
-        health = cardList[0].health;
-        cardDescription = cardList[0].cardDescription;
-        spriteImage = cardList[0].spriteImage;
 
-
-        nameText.text = " " + cardName;
-        costText.text = " " + cost;
-        powerText.text = " " + power;
-        healthText.text = " " + health;
-        descriptionText.text = " " + cardDescription;
-        artImage.sprite = spriteImage;
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        staticCardBack = cardBack;
-
-    }
 }
