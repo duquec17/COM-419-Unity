d95ed9b1f0be21b86305c62a4374aa3abb362f78
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-13 19:42:59 -0500

    Adjusting turn system

--- Card Prototype/Assets/Scripts/turnsystem.cs
+++ Card Prototype/Assets/Scripts/turnsystem.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 
 public class TurnSystem : MonoBehaviour
 {
-    public bool isYourTurn;
+    public enum TurnPhase { Player, Opponent, Battle };
+
+    public TurnPhase currentPhase;
     public int yourTurn;
     public int isOpponentTurn;
     public Text turnText;
@@ -14,43 +16,69 @@ public class TurnSystem : MonoBehaviour
     public int currentMana;
     public Text manaText;
     internal static bool startTurn;
+    private bool isPlayerFirst;
 
     void Start()
     {
-        isYourTurn = true;
         yourTurn = 1;
         isOpponentTurn = 0;
-
         maxMana = 1;
         currentMana = 1;
+        currentPhase = TurnPhase.Player;
+        isPlayerFirst = true; // Assuming player goes first initially
     }
 
     void Update()
     {
-        if(isYourTurn == true)
+        switch (currentPhase)
         {
-            turnText.text = "Your Turn";
+            case TurnPhase.Player:
+                turnText.text = "Your Turn";
+                break;
+            case TurnPhase.Opponent:
+                turnText.text = "Opponent Turn";
+                break;
+            case TurnPhase.Battle:
+                turnText.text = "Battle Phase";
+                break;
+        }
+        if (manaText != null)
+            manaText.text = currentMana + "/" + maxMana;
+    }
 
-        }
-        else{
-            turnText.text = "Opponent Turn";
-        
-        }
-         manaText.text = currentMana + "/" + maxMana;
-     }
-  
     public void EndYourTurn()
     {
-        isYourTurn = false;
-        isOpponentTurn += 1;
+        if (currentPhase == TurnPhase.Player)
+        {
+            currentPhase = TurnPhase.Opponent;
+            isOpponentTurn++;
+        }
     }
 
     public void EndOpponentTurn()
     {
-        isYourTurn = true;
-        yourTurn += 1;
+        if (currentPhase == TurnPhase.Opponent)
+        {
+            currentPhase = TurnPhase.Battle;
+        }
+        else if (currentPhase == TurnPhase.Battle)
+        {
+            currentPhase = TurnPhase.Player;
+            yourTurn++;
 
-        maxMana += 1;
-        currentMana = maxMana;
+            maxMana++;
+            currentMana = maxMana;
+
+            if (!isPlayerFirst) // If the player didn't go first in the beginning
+            {
+                currentPhase = TurnPhase.Opponent; // Switch to opponent's turn
+                isOpponentTurn++;
+            }
+        }
+    }
+
+    public void SetPlayerFirst(bool isFirst)
+    {
+        isPlayerFirst = isFirst;
     }
 }
