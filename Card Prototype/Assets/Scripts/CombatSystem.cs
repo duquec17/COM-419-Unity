5cbb8327c73c3612aff0dd2123f47f938b27d727
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-13 19:12:38 -0500

    Combat System components

--- Card Prototype/Assets/Scripts/CombatSystem.cs
+++ Card Prototype/Assets/Scripts/CombatSystem.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CombatSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public DropZone allyDropZone;
+    public DropZone enemyDropZone;
+
     void Start()
     {
-        
+        // Assuming this script is attached to a GameObject in the scene,
+        // you can get references to the DropZone components via the GameObjects' tags.
+        allyDropZone = GameObject.FindGameObjectWithTag("AllyDropZone").GetComponent<DropZone>();
+        enemyDropZone = GameObject.FindGameObjectWithTag("EnemyDropZone").GetComponent<DropZone>();
+
+        // Start combat at the beginning of the turn
+        StartCombat();
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartCombat()
     {
-        
+        // Check if both drop zones are not empty
+        if (allyDropZone.HasCard() && enemyDropZone.HasCard())
+        {
+            // Get the cards from both drop zones
+            Card allyCard = allyDropZone.GetCard();
+            Card enemyCard = enemyDropZone.GetCard();
+
+            // Resolve combat between the two cards
+            CombatResult result = CombatResolver.ResolveCombat(allyCard, enemyCard);
+
+            // Apply the combat result
+            ApplyCombatResult(result);
+        }
+    }
+
+    void ApplyCombatResult(CombatResult result)
+    {
+        // Implement logic to apply the combat result
     }
 }
