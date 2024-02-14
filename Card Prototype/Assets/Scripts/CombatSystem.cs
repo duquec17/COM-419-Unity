71614aea2cbd8aac1b82f4570dcd66a0fbec53bb
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-14 11:18:25 -0500

    Update CombatSystem.cs

--- Card Prototype/Assets/Scripts/CombatSystem.cs
+++ Card Prototype/Assets/Scripts/CombatSystem.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq; // Import LINQ namespace
 
 public class CombatSystem : MonoBehaviour
 {
-    public DropZone allyDropZone;
-    public DropZone enemyDropZone;
-    public TurnSystem turnSystem; // Reference to the TurnSystem script
+    public DropZone[] allyDropZones; // Array to hold ally drop zones
+    public DropZone[] enemyDropZones; // Array to hold enemy drop zones
+    public TurnSystem turnSystem;
 
     void Start()
     {
-        // Get references to DropZone components
-        allyDropZone = GameObject.FindGameObjectWithTag("AllyDropZone")?.GetComponent<DropZone>();
-        enemyDropZone = GameObject.FindGameObjectWithTag("EnemyDropZone")?.GetComponent<DropZone>();
+        // Find all ally and enemy drop zones in the scene and convert them to DropZone array
+        allyDropZones = GameObject.FindGameObjectsWithTag("AllyDropZone").Select(obj => obj.GetComponent<DropZone>()).ToArray();
+        enemyDropZones = GameObject.FindGameObjectsWithTag("EnemyDropZone").Select(obj => obj.GetComponent<DropZone>()).ToArray();
 
         // Get reference to the TurnSystem script
         turnSystem = FindObjectOfType<TurnSystem>();
@@ -20,6 +21,7 @@ public class CombatSystem : MonoBehaviour
         // Subscribe to the OnPhaseChange event of the TurnSystem
         turnSystem.OnPhaseChange += HandlePhaseChange;
     }
+
 
     void HandlePhaseChange(TurnSystem.TurnPhase newPhase)
     {
@@ -33,34 +35,49 @@ public class CombatSystem : MonoBehaviour
 
     void StartCombat()
     {
-        Debug.Log("Interal code of StartCombat() Begins");
+        Debug.Log("Combat begins");
 
-        // Check if both drop zones are not empty
-        Debug.Log("Ally Drop Zone has card: " + allyDropZone.HasCard());
-        Debug.Log("Enemy Drop Zone has card: " + enemyDropZone.HasCard());
+        // Check if both ally and enemy drop zones are not empty
+        bool allyHasCard = CheckDropZonesNotEmpty(allyDropZones);
+        bool enemyHasCard = CheckDropZonesNotEmpty(enemyDropZones);
 
-        // Check if both drop zones are not empty
-        if (allyDropZone.HasCard() && enemyDropZone.HasCard())
+        if (allyHasCard && enemyHasCard)
         {
-            // Get the cards from both drop zones
-            Card allyCard = allyDropZone.GetCard();
-            Card enemyCard = enemyDropZone.GetCard();
+            // Get cards from drop zones
+            Card allyCard = GetRandomCardFromDropZones(allyDropZones);
+            Card enemyCard = GetRandomCardFromDropZones(enemyDropZones);
 
             // Resolve combat between the two cards
             CombatResult result = CombatResolver.ResolveCombat(allyCard, enemyCard);
 
             // Apply the combat result
             ApplyCombatResult(result);
-
-            Debug.Log("CombatResults have been applied or attempted to be");
         }
         else
         {
             // If either drop zone is empty, end the turn
             turnSystem.EndOpponentTurn();
-
-            Debug.Log("End battle phase or attempted to be");
         }
+    }
+
+    bool CheckDropZonesNotEmpty(DropZone[] dropZones)
+    {
+        foreach (DropZone dropZone in dropZones)
+        {
+            if (!dropZone.HasCard())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    Card GetRandomCardFromDropZones(DropZone[] dropZones)
+    {
+        // Get a random drop zone
+        DropZone dropZone = dropZones[Random.Range(0, dropZones.Length)];
+        // Get the card from the drop zone
+        return dropZone.GetCard();
     }
 
     void ApplyCombatResult(CombatResult result)
