1d7210caccdd75b06ec9f486a8665390fa168ef2
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-16 09:40:50 -0500

    LocalPlayer script
    Eases use development of other network scripts

--- Card Prototype/Assets/Scripts/Draggable.cs
+++ Card Prototype/Assets/Scripts/Draggable.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Mirror;
 
-public class Draggable : NetworkBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    //Variable list
     public Transform parentToReturnTo = null;
-    private Vector3 startPosition;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        if (!isLocalPlayer)
-            return;
-
         Debug.Log("BeginDrag");
 
         parentToReturnTo = this.transform.parent;
@@ -25,37 +20,28 @@ public class Draggable : NetworkBehaviour, IBeginDragHandler, IDragHandler, IEnd
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (!isLocalPlayer)
-            return;
-
         this.transform.position = eventData.position;  
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (!isLocalPlayer)
-            return;
-
         Debug.Log("EndDrag");
         this.transform.SetParent(parentToReturnTo);
 
         GetComponent<CanvasGroup>().blocksRaycasts = true;
 
-        // Call an RPC to synchronize the position of the card with all clients
-        CmdUpdateCardPosition(this.transform.position);
     }
 
-    [Command]
-    private void CmdUpdateCardPosition(Vector3 newPosition)
+
+    // Start is called before the first frame update
+    void Start()
     {
-        RpcUpdateCardPosition(newPosition);
+        
     }
 
-    [ClientRpc]
-    private void RpcUpdateCardPosition(Vector3 newPosition)
+    // Update is called once per frame
+    void Update()
     {
-        // Update the position of the card on all clients except the local player
-        if (!isLocalPlayer)
-            this.transform.position = newPosition;
+        
     }
 }
