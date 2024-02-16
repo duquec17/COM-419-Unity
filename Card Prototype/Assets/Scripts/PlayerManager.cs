10831334f8107ec01cb06f47a6c59f1b32997a8c
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-16 17:49:25 -0500

    adj

--- Card Prototype/Assets/Scripts/PlayerManager.cs
+++ Card Prototype/Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerManager : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject AllyDropZone;
+    public GameObject AllyHand;
+    public GameObject EnemyDropZone;
+    public GameObject EnemyHand;
+
 }
