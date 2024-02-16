51ccbe741470be8f0a6d0d3f90acf1726bad0760
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-16 18:26:28 -0500

    Update PlayerManager.cs

--- Card Prototype/Assets/Scripts/PlayerManager.cs
+++ Card Prototype/Assets/Scripts/PlayerManager.cs
@@ -24,11 +24,12 @@ public class PlayerManager : NetworkBehaviour
         EnemyDropZone = GameObject.Find("EnemyDropZone");
     }
 
+    [Server]
     public override void OnStartServer()
     {
         base.OnStartServer();
 
         cardList.Add(new Card(0, "Jester", 1, 1, 2, "+1 power for every other Jester card on the board", Resources.Load<Sprite>("placeHolder")));
-
+        Debug.Log(cardList);
     }
 }
