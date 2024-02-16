974ff0fbf7176f13af1fee385e2cf8e1411c4bd1
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-16 18:24:44 -0500

    Player Manager

--- Card Prototype/Assets/Scripts/PlayerManager.cs
+++ Card Prototype/Assets/Scripts/PlayerManager.cs
@@ -6,10 +6,13 @@ using Mirror;
 public class PlayerManager : NetworkBehaviour
 {
     //List of network shared objects
+    public GameObject PlayerCard;
     public GameObject AllyDropZone;
     public GameObject AllyHand;
     public GameObject EnemyDropZone;
     public GameObject EnemyHand;
+
+    public static List<Card> cardList = new List<Card>();
 
     public override void OnStartClient()
     {
@@ -19,5 +22,13 @@ public class PlayerManager : NetworkBehaviour
         EnemyHand = GameObject.Find("EnemyHand");
         AllyDropZone = GameObject.Find("AllyDropZone");
         EnemyDropZone = GameObject.Find("EnemyDropZone");
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        cardList.Add(new Card(0, "Jester", 1, 1, 2, "+1 power for every other Jester card on the board", Resources.Load<Sprite>("placeHolder")));
+
     }
 }
