8a3e3a29b5d61c33a40bb9bf30b003428741f6b4
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-16 09:06:02 -0500

    Draggable Modification to work online

--- Card Prototype/Assets/Scripts/Draggable.cs
+++ Card Prototype/Assets/Scripts/Draggable.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Mirror;
 
-public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Draggable : NetworkBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    //Variable list
     public Transform parentToReturnTo = null;
+    private Vector3 startPosition;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        if (!isLocalPlayer)
+            return;
+
         Debug.Log("BeginDrag");
 
         parentToReturnTo = this.transform.parent;
@@ -19,28 +25,37 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (!isLocalPlayer)
+            return;
+
         this.transform.position = eventData.position;  
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (!isLocalPlayer)
+            return;
+
         Debug.Log("EndDrag");
         this.transform.SetParent(parentToReturnTo);
 
         GetComponent<CanvasGroup>().blocksRaycasts = true;
 
+        // Call an RPC to synchronize the position of the card with all clients
+        CmdUpdateCardPosition(this.transform.position);
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    [Command]
+    private void CmdUpdateCardPosition(Vector3 newPosition)
     {
-        
+        RpcUpdateCardPosition(newPosition);
     }
 
-    // Update is called once per frame
-    void Update()
+    [ClientRpc]
+    private void RpcUpdateCardPosition(Vector3 newPosition)
     {
-        
+        // Update the position of the card on all clients except the local player
+        if (!isLocalPlayer)
+            this.transform.position = newPosition;
     }
 }
