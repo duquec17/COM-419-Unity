f6f222737ad6c7f776a9c23f9815935b41df9e18
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-17 14:59:23 -0500

    testing

--- Card Prototype/Assets/Scripts/DrawCards.cs
+++ Card Prototype/Assets/Scripts/DrawCards.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class DrawCards : MonoBehaviour
 {
     public GameObject PlayerCard;
+    public GameObject AllyHand;
 
     public void OnClick()
     {
         for(int i = 0; i < 3; i++)
         {
             GameObject card = Instantiate(PlayerCard, new Vector2(0,0), Quaternion.identity);
+            card.transform.SetParent(AllyHand.transform, false);
             Debug.Log("Drawing card" + i);
         }
     }
