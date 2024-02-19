be54c014ce2d7e7c1a5ee497103f5f0dbae183a6
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-02-19 16:03:44 -0500

    Update Draggable.cs

--- Card Prototype/Assets/Scripts/Draggable.cs
+++ Card Prototype/Assets/Scripts/Draggable.cs
@@ -31,17 +31,4 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
         GetComponent<CanvasGroup>().blocksRaycasts = true;
 
     }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
