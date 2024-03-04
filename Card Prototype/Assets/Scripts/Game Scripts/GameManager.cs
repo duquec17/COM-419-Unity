1797a83f8f9024acdb46020bbe00588d42200f36
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-03-04 09:49:37 -0500

    Update GameManager.cs

--- Card Prototype/Assets/Scripts/Game Scripts/GameManager.cs
+++ Card Prototype/Assets/Scripts/Game Scripts/GameManager.cs
@@ -5,12 +5,15 @@ using Mirror;
 
 public class GameManager : NetworkBehaviour
 {
+    //Variables managing game logic
     public int TurnOrder = 0;
     public string GameState = "Initialize {}";
     public int AllyMana = 0;
     public int EnemyMana = 0;
     public int AllyVariables = 0;
     public int EnemyVariables = 0;
+
+    private int ReadyClicks = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +25,15 @@ public class GameManager : NetworkBehaviour
     {
         if (stateRequest == "Initialize {}")
         {
-
+            ReadyClicks = 0;
+            GameState = "Initialize {}";
         }
         else if (stateRequest == "Complie {}")
         {
-
+            if (ReadyClicks == 1)
+            {
+                GameState = "Compile {}";
+            }
         }
         else if (stateRequest == "Execute {}")
         {
