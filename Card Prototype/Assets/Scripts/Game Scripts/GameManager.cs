8c51dfac23b289c6e4904a62b876760bbc124f0b
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-03-04 09:05:46 -0500

    Organizing code

--- Card Prototype/Assets/Scripts/Game Scripts/GameManager.cs
+++ Card Prototype/Assets/Scripts/Game Scripts/GameManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
