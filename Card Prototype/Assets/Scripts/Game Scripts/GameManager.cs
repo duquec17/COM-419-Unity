8660121315b96c5c86ed74ba310cc819a688797a
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-03-04 09:10:37 -0500

    Update GameManager.cs

--- Card Prototype/Assets/Scripts/Game Scripts/GameManager.cs
+++ Card Prototype/Assets/Scripts/Game Scripts/GameManager.cs
@@ -5,6 +5,13 @@ using Mirror;
 
 public class GameManager : NetworkBehaviour
 {
+    public int TurnOrder = 0;
+    public string GameState = "Initialize {}";
+    public int AllyMana = 0;
+    public int EnemyMana = 0;
+    public int AllyVariables = 0;
+    public int EnemyVariables = 0;
+
     // Start is called before the first frame update
     void Start()
     {
