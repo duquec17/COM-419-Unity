76d6c04fe55f6307f73632b6cc0f4f139994f98e
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-03-04 22:13:40 -0500

    Update PlayerManager.cs

--- Card Prototype/Assets/Scripts/PlayerManager.cs
+++ Card Prototype/Assets/Scripts/PlayerManager.cs
@@ -172,11 +172,11 @@ public class PlayerManager : NetworkBehaviour
         {
             if (isOwned)
             {
-                //card.transform.SetParent(AllyDropZone.transform, false);
+                card.transform.SetParent(AllyDropZone.transform, false);
             }
             else
             {
-                //card.transform.SetParent(EnemyDropZone.transform, false);
+                card.transform.SetParent(EnemyDropZone.transform, false);
             }
 
         }
