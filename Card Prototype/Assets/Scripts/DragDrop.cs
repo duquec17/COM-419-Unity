4d6d16f0524fd28787661b019165cc4bb81be5ac
Cristian Duque <122939979+duquec17@users.noreply.github.com>
2024-03-06 16:54:10 -0500

    Alternative Drag method

---
+++ Card Prototype/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector2 startPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    // Start is called before the first frame update
+    public void StartDrag()
+    {
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+    }
+    
+}
